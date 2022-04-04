8f44ece7e02c2ad2961ae9f803fc88396fc9b114
Andrew Tribble <andrewtr001@gmail.com>
2022-04-03 21:57:07 -0500

    Update blueytemplate.user.js

--- blueytemplate.user.js
+++ blueytemplate.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bluey Place overlay
 // @namespace    https://nightc.at/
-// @version      1.6
+// @version      1.8
 // @description  Bluey Template
 // @author       NameWasAlreadyTaken
 // @match        https://hot-potato.reddit.com/embed*
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/9a60046580264742952/960371472398946364/BlueySitDraft3.png";
+                i.src = "https://cdn.discordapp.com/attachments/960046580264742952/960371472398946364/BlueySitDraft3.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
