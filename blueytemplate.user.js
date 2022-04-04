5cdce52b6aab58a8f0a8ffc4c2ac92b80002b098
Andrew Tribble <andrewtr001@gmail.com>
2022-04-03 21:54:25 -0500

    Update blueytemplate.user.js

--- blueytemplate.user.js
+++ blueytemplate.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/960191135257604126/960275831110905856/BlueyFront.png";
+                i.src = "https://cdn.discordapp.com/attachments/9a60046580264742952/960371472398946364/BlueySitDraft3.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
